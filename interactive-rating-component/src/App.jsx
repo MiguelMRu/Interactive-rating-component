8057f32bf45e963172c5a5a925904bd5d93ad4af
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-07-02 07:22:24 +0200

    primeros cambios

--- interactive-rating-component/src/App.jsx
+++ interactive-rating-component/src/App.jsx
@@ -1,34 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import star from "./assets/icon-star.svg"
+import { Thanks } from './components/Thanks'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const rating = [1,2,3,4,5]
+  const [selected, setSelected] = useState()
+
+  const selectClass = selected ? 'rating-button selected' : 'rating-button'
+
+  const handleClick = (number) =>{
+    setSelected(number)
+
+  }
+   
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+
+    <main>
+      <div className='star-container'>
+        <img src={star} alt="star icon" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      
+      <h1>How did we do?</h1>
+
+      <p>Please let us know how we did with your support request. All feedback is appreciated 
+      to help us improve our offering!</p>
+
+
+      <div>     
+      {
+        rating.map((num) => (
+          <button
+            key={num}
+            className={selectClass}
+            onClick={() => handleClick(num)}
+
+            >
+            {num}
+          </button>
+
+        ))
+      }
+      
+    </div>
+
+      <form >
+      <button type="submit">SUBMIT</button>
+      </form>
+
+      {selected ? <Thanks rating={selected}/> : ''}
+
+    </main>
   )
 }
 
