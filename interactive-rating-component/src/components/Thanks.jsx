3ef2e2a1681daa1c101a262a22a4c45cd83c89e9
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-07-02 07:22:52 +0200

    componente de gracias

---
+++ interactive-rating-component/src/components/Thanks.jsx
@@ -0,0 +1,14 @@
+export function Thanks({rating}){
+
+
+    return(
+    <section>
+        <p><strong>You selected {rating} out of 5</strong></p>
+
+        <h2>Thank you!</h2>
+
+        <p>We appreciate you taking the time to give a rating. If you ever need more support, 
+        don’t hesitate to get in touch!</p>
+      </section>
+    )
+}
